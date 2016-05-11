5a3790818f326ab638c245b7b6c94f210c163667
Eric Kizaki <erickizaki@gmail.com>
2016-05-10 22:25:19 -0500

    Refactor to 1.5 component

--- client/app/blog/blog.follow.directive.js
+++ client/app/blog/blog.follow.directive.js
@@ -1,51 +1,42 @@
 (function() {
     'use strict';
 
-    angular.module('angularFullApp').directive('blogFollow', blogFollow);
+    angular.module('angularFullApp').component('blogFollow', {
+        templateUrl: 'app/blog/blog.follow.html',
+        bindings: {},
+        controller: FollowController,
+        controllerAs: 'vm'
+    });
 
-    function blogFollow(ValidationService, BlogService) {
-        var directive = {
-            restrict: 'E',
-            templateUrl: 'app/blog/blog.follow.html',
-            scope: {},
-            controller: FollowController,
-            controllerAs: 'vm',
-            bindToController: true
+    function FollowController(ValidationService, BlogService) {
+        var vm = this;
+        vm.save = save;
+        vm.follow = {
+            followers: 0,
+            email: ''
         };
 
-        return directive;
-
-        function FollowController() {
-            var vm = this;
-            vm.save = save;
-            vm.follow = {
-                followers: 0,
-                email: ''
-            };
-
-            function getMailListTotal() {
-
-                BlogService.getMailingListTotal().$promise.then(function(totalObj) {
-                    vm.follow.followers = totalObj.total;
-                });
-            }
-
-            function save(form) {
-                if (form.$valid) {
-
-                    BlogService.subscribeToMailingList({
-                        email: vm.follow.email
-                    }).$promise.then(function() {
-                        ValidationService.success('You have been added to the mailing list');
-
-                        getMailListTotal();
-                    });
-
-                }
-            }
-
-            getMailListTotal();
+        function getMailListTotal() {
+            BlogService.getMailingListTotal().$promise.then(function(totalObj) {
+                vm.follow.followers = totalObj.total;
+            });
         }
+
+        function save(form) {
+            if (form.$valid) {
+
+                BlogService.subscribeToMailingList({
+                    email: vm.follow.email
+                }).$promise.then(function() {
+                    ValidationService.success('You have been added to the mailing list');
+
+                    getMailListTotal();
+                });
+
+            }
+        }
+
+        getMailListTotal();
     }
 
 })();
