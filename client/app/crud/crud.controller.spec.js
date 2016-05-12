745722d7b9e9ab47fe9d8fd4d6edb29d8aedd986
Eric Kizaki <erickizaki@gmail.com>
2016-05-11 19:17:59 -0500

    refactor rename

--- client/app/crud/crud.controller.spec.js
+++ client/app/crud/crud.controller.spec.js
@@ -1,21 +1,21 @@
 'use strict';
 
-describe('Controller: CrudCtrl', function () {
+describe('Controller: CrudController', function() {
 
-  // load the controller's module
-  beforeEach(module('angularFullApp'));
+    // load the controller's module
+    beforeEach(module('angularFullApp'));
 
-  var CrudCtrl, scope;
+    var CrudController, scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    CrudCtrl = $controller('CrudCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        CrudController = $controller('CrudController', {
+            $scope: scope
+        });
+    }));
+
+    it('should ...', function() {
+        expect(1).toEqual(1);
     });
-  }));
-
-  it('should ...', function () {
-    expect(1).toEqual(1);
-  });
 });
