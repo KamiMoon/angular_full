ae53eb75ff38a8682d53a332e6395c5130a9f159
Eric Kizaki <erickizaki@gmail.com>
2016-05-11 19:27:56 -0500

    refactor

--- client/components/bFeedback/bFeedback.directive.js
+++ client/components/bFeedback/bFeedback.directive.js
@@ -1,15 +1,19 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('angularFullApp')
-    .directive('bFeedback', function() {
+    angular.module('angularFullApp')
+        .directive('bFeedback', bFeedback);
+
+    function bFeedback() {
         return {
+            restrict: 'E',
             scope: false,
             replace: true,
             template: '<div ng-if="bFeedbackMessage && bFeedbackMessage.length > 0" ' +
                 'class="alert alert-{{bFeedbackMessageClass}} alert-dismissible" role="alert">{{bFeedbackMessage}}' +
                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-                '<span aria-hidden="true">&times;</span></button></div>',
-            restrict: 'E' //,
-                //link: function postLink(scope, element, attrs) {}
+                '<span aria-hidden="true">&times;</span></button></div>'
         };
-    });
+    }
+
+})();
